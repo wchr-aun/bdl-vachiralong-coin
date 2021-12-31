c26b2e044f9be994f05088c48f9e939f04049c96
wchr-aun <wchr.aun@gmail.com>
2021-12-31 04:08:45 +0000

    :sparkles: (masterContract) to implement the core functions

--- contracts/masterContract.sol
+++ contracts/masterContract.sol
@@ -1,4 +1,10 @@
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract Custom_Send {
+    function customSend(uint256 value, address receiver) public returns (bool) {}
+}
 
 contract Vachiralong_Coin {
   uint256 public tokenPrice;
@@ -10,29 +16,56 @@ contract Vachiralong_Coin {
 
   address public owner;
 
-  constructor() {
-      owner = msg.sender;
+  Custom_Send _customSend;
+
+  modifier isOwner() {
+    require(msg.sender == owner, "Unauthorised");
+    _;
   }
 
-  function buyToken(uint256 amount) public {
+  constructor(uint256 initPrice, address customAddr) {
+      owner = msg.sender;
+      balances[msg.sender] = initPrice * 10 ether;
+      tokenPrice = initPrice;
+      _customSend = Custom_Send(customAddr);
+  }
+
+  function buyToken(uint256 amount) public payable returns (bool) {
+    require(msg.value == amount * tokenPrice && amount > 0);
+    balances[msg.sender] += amount;
+    tokenPrice *= 2;
+    emit Price(tokenPrice);
     emit Purchase(msg.sender, amount);
+    return true;
   }
   
-  function transfer(address recipient, uint256 amount) public {
+  function transfer(address recipient, uint256 amount) public returns (bool) {
+    require(balances[msg.sender] >= amount && amount > 0);
+    balances[msg.sender] -= amount;
+    balances[recipient] += amount;
     emit Transfer(msg.sender, recipient, amount);
+    return true;
   }
 
-  function sellToken(uint256 amount) public {
+  function sellToken(uint256 amount) public returns (bool) {
+    require(balances[msg.sender] >= amount && amount > 0);
+    balances[msg.sender] -= amount;
+    _customSend.customSend(amount * tokenPrice, msg.sender);
     emit Sell(msg.sender, amount);
+    return true;
   }
 
-  function changePrice(uint256 price) public {
-    require(msg.sender == owner);
+  function changePrice(uint256 price) public isOwner returns (bool) {
     tokenPrice = price;
     emit Price(tokenPrice);
+    return true;
   }
 
   function getBalance() public view returns (uint256) {
     return balances[msg.sender];
+  }
+
+  function setCustomLib(address addr) public isOwner {
+    _customSend = Custom_Send(addr);
   }
 }
